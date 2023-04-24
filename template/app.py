d2fcd5c51c7495bda92d4b1d276076a376c47ab2
kartik1502 <kartikkulkarni1411@gmail.com>
2023-04-24 10:34:49 +0530

    added the layout

--- template/app.py
+++ template/app.py
@@ -5,7 +5,7 @@ from streamlit_option_menu import option_menu
 import streamlit_toggle as tog
 import functionalities
 
-st.set_page_config(page_title="Disease Detection Application", page_icon="https://img.icons8.com/color/48/null/caduceus.png")
+st.set_page_config(page_title="Disease Detection Application", page_icon="https://img.icons8.com/color/48/null/caduceus.png", layout="wide")
 
 with st.sidebar:
     choice = option_menu('Multiple Disease Detection',['Chronic Kidney Disease','Parkinson\'s Disease'])
